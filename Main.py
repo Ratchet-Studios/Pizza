9e82de6c2ec841a1098d428bca773e460287aa61
Luc Hayward <luchay10@gmail.com>
2018-02-04 14:50:22 +0200

    sectioned

--- Main.py
+++ Main.py
@@ -3,3 +3,17 @@
 # A slice has no holes and is rpresented by two rows and columns
 # Slices must contain L cells of each ingredient and at most H in total
 # No overlapping
+
+#Boyd
+
+
+
+
+
+
+#Stu
+
+
+
+
+#Luc
