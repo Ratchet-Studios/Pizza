c760cfef6e74323e52636b8f739ce9ce58c774b8
Beyarkay <boydrkane@gmail.com>
2018-02-04 14:54:43 +0100

    Created the Pizza.class and added Stu's slice checker to it

--- Main.py
+++ Main.py
@@ -9,51 +9,48 @@
 
 
 #Final Code:
+class Pizza(object):
+    def __init__(self, file):
+        is_first_line = True
+        self.PIZZA = []
+        self.TOT_MUSHROOMS = 0
+        self.TOT_TOMATOES = 0
+        for row_index, row in enumerate(file):
+            if is_first_line:
+                data = row.split(" ")
+                self.MIN_TOPPINGS = int(data[2])
+                self.MAX_CELLS = int(data[3].strip())
+                is_first_line = False
+            else:
+                self.PIZZA.append([])
+                for char_index, char in enumerate(row):
+                    if char_index < len(row)-1:
+                        self.PIZZA[row_index-1].append(char)
+                        if char == "M":
+                            self.TOT_MUSHROOMS += 1
+                        elif char == "T":
+                            self.TOT_TOMATOES += 1
+        self.used = []
+        for i in range(len(self.PIZZA)):
+            self.used.append([])
+            for j in range(len(self.PIZZA[i])):
+                self.used[i].append(False)
 
+    def print(self):
+        print("Pizza=")
+        for row in self.PIZZA:
+            print(str(row))
+        print("mushrooms={0}, tomatoes={1}, max_cells={2}, min_toppings={3}")
 
-
-
-
-#Boyd's Workspace
+    def print_used(self):
+        for row in self.used:
+            print(str(row))
 
 
 file = open("example.in")
-PIZZA = []
-is_first_line = True
-TOT_MUSHROOMS = 0
-TOT_TOMATOES = 0
-global MAX_CELLS
-global MIN_TOPPINGS
-for row_index, row in enumerate(file):
-    if is_first_line:
-        data = row.split(" ")
-        MIN_TOPPINGS = int(data[2])
-        MAX_CELLS = int(data[3].strip())
-        is_first_line = False
-    else:
-        PIZZA.append([])
-        for char_index, char in enumerate(row):
-            if char_index < len(row)-1:
-                PIZZA[row_index-1].append(char)
-                if char == "M":
-                    TOT_MUSHROOMS += 1
-                elif char == "T":
-                    TOT_TOMATOES += 1
+pizza = Pizza(file)
+pizza.print()
 
-def print_pizza():
-    for row in PIZZA:
-        print(str(row))
-    
-    
-used = []
-for i in range(len(PIZZA)):
-    used.append([])
-    for j in range(len(PIZZA[i])):
-        used[i].append(False)
-
-def print_used():
-    for row in used:
-        print(str(row))
 
 
 
