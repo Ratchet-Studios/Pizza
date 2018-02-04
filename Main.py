beb5db95765771eb4c283f97a19f6c90fe4fddcb
Luc Hayward <luchay10@gmail.com>
2018-02-04 14:46:50 +0200

    Added rules

---
+++ Main.py
@@ -0,0 +1,5 @@
+# Pizza represented as 2D array
+# Each cell contains either a (M)ushroom or a (T)omato
+# A slice has no holes and is rpresented by two rows and columns
+# Slices must contain L cells of each ingredient and at most H in total
+# No overlapping
